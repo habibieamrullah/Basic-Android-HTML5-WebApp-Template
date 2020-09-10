baad2ae3b8c9a35d86289fd12705c68a6e36c117
Habibie <habibieamrullah@gmail.com>
2020-09-10 22:09:50 +0700

    Better!

--- app/src/main/assets/awebapp.js
+++ app/src/main/assets/awebapp.js
@@ -1,12 +1,20 @@
-function showBanner(){
-	try{
-		Android.showAndroidBanner()
-	}catch(e){
-		console.log(e)
-	}
+var awebapp = {}
+
+//Show banner ad
+awebapp.showBanner = function (){
+    if(!awebapp.ispro){
+        try{
+            Android.showAndroidBanner()
+        }catch(e){
+            console.log(e)
+        }
+    }else{
+        console.log("App is Pro")
+    }
 }
 
-function hideBanner(){
+//Hide banner ad
+awebapp.hideBanner = function (){
 	try{
 		Android.hideAndroidBanner()
 	}catch(e){
@@ -14,7 +22,39 @@ function hideBanner(){
 	}
 }
 
-function landscape(){
+//Show interstitial ad
+awebapp.showInterstitial = function (){
+    if(!awebapp.ispro){
+        try{
+            Android.showAd()
+        }catch(e){
+            console.log(e)
+        }
+    }else{
+        console.log("App is Pro")
+    }
+}
+
+awebapp.showRewardedAd = function(){
+    if(!awebapp.ispro){
+        try{
+            Android.showRewardedAd()
+        }catch(e){
+            console.log(e)
+        }
+    }else{
+        console.log("App is Pro")
+    }
+}
+
+//User got reward from rewarded ad
+awebapp.rewarded = function(){
+    //Modify this as you need
+    document.getElementById("currentrewards").innerHTML = "Got ad Reward!"
+}
+
+//Landscape screen
+awebapp.landscape = function (){
 	try{
 		Android.landscape()
 	}catch(e){
@@ -22,7 +62,8 @@ function landscape(){
 	}
 }
 
-function portrait(){
+//Portrait screen
+awebapp.portrait = function (){
 	try{
 		Android.portrait()
 	}catch(e){
@@ -30,10 +71,58 @@ function portrait(){
 	}
 }
 
-function showInterstitial(){
-	try{
-		Android.showAd()
-	}catch(e){
-		console.log(e)
-	}
+//Show toast alert
+awebapp.alert = function(txt){
+    try{
+        Android.showAlert(txt)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+//Rate this app
+awebapp.rateThisApp = function(){
+    try{
+        Android.rateThisApp()
+    }catch(e){
+        console.log(e)
+    }
+}
+
+//Share a text
+awebapp.shareText = function(txt, title){
+    try{
+        Android.shareText(txt, title)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+//Share this app
+awebapp.shareThisApp = function(txt, title){
+    try{
+        Android.shareThisApp(txt, title)
+    }catch(e){
+        console.log(e)
+    }
+}
+
+//Purchase Remove Ads feature
+awebapp.removeAds = function(){
+    try{
+        Android.removeAds()
+    }catch(e){
+        console.log(e)
+    }
+}
+
+//Determine the app is it pro without ads or not
+awebapp.ispro = false
+awebapp.itspro = function(n){
+    if(n == 1){
+        awebapp.hideBanner()
+        awebapp.ispro = true
+    }else{
+        awebapp.ispro = false
+    }
 }
--- app/src/main/java/com/ciihuy/basicandroidhtml5webapp/MainActivity.java
+++ app/src/main/java/com/ciihuy/basicandroidhtml5webapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ciihuy.basicandroidhtml5webapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -17,25 +21,42 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
 
+    //AdMob ads related variables
     AdView mAdView;
     private InterstitialAd mInterstitialAd;
     boolean intCanShow = true;
     boolean bannerCanShow = false;
+    boolean rewardedAdCanShow = false;
     boolean mustDestroyBanner = false;
+    private RewardedAd rewardedAd;
 
+    //In App Purchase related variables
+    boolean ispro = false; //pro means ads are removed by user purchase
+    BillingProcessor bp;
+    SharedPreferences sharedpreferences;
+
+    //Webview related variables
     ProgressDialog progress;
-
     WebView browser;
 
     @Override
@@ -72,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url){
                 progress.dismiss();
+                if(ispro){
+                    browser.loadUrl("javascript:awebapp.itspro(1)");
+                }
             }
 
 
@@ -82,12 +106,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Mobile Ads Init
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
 
+        //Banner ads init
         mAdView = findViewById(R.id.adView);
         mAdView.setAdListener(new AdListener() {
             @Override
@@ -123,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Interstitial ads init
         mInterstitialAd = new InterstitialAd(this);
         mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
         mInterstitialAd.setAdListener(new AdListener() {
@@ -158,13 +185,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        initAd();
-        initBanner();
+        //RewardedAd First Init
+        initRewardedAd();
+
+        //Hide the banner first...
         destroyBanner();
+
+        //Billing Processor
+        bp = new BillingProcessor(MainActivity.this, "your licensekey from Google Play Devoloper Console", this);
+        bp.initialize();
+
+        sharedpreferences = getApplicationContext().getSharedPreferences("sharedprefttssjawa", MODE_PRIVATE);
+        if(sharedpreferences.contains("ispro")) {
+            ispro = sharedpreferences.getBoolean("ispro", false);
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(!ispro){
+                    //First init of ads
+                    initAd();
+                    destroyBanner();
+                    browser.loadUrl("javascript:awebapp.itspro(0)");
+                }else{
+                    browser.loadUrl("javascript:awebapp.itspro(1)");
+                }
+
+            }
+        }, 5000);
     }
 
 
+    //Run Ad Loop
     public void initAd(){
+        //Interstitial
         if(intCanShow){
             if (mInterstitialAd.isLoaded()) {
                 mInterstitialAd.show();
@@ -172,15 +226,8 @@ public class MainActivity extends AppCompatActivity {
             mInterstitialAd.loadAd(new AdRequest.Builder().build());
         }
         intCanShow = false;
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                initAd();
-            }
-        }, 1000);
-    }
 
-    public void initBanner(){
+        //Banner
         if(bannerCanShow){
             showBanner();
         }
@@ -189,21 +236,120 @@ public class MainActivity extends AppCompatActivity {
             destroyBanner();
         }
         mustDestroyBanner = false;
+
+        //Rewarded
+        if(rewardedAdCanShow){
+            loadRewardedVideoAd();
+        }
+        rewardedAdCanShow = false;
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                initBanner();
+                initAd();
             }
         }, 1000);
     }
 
+    //Show banner ad
     public void showBanner(){
         mAdView.loadAd(new AdRequest.Builder().build());
         mAdView.setVisibility(View.VISIBLE);
     }
 
+    //Hide banner ad
     public void destroyBanner(){
         mAdView.setVisibility(View.GONE);
+    }
+
+    //Init Rewarded Ad
+    private void initRewardedAd(){
+        rewardedAd = new RewardedAd(this,"ca-app-pub-3940256099942544/5224354917");
+        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
+            @Override
+            public void onRewardedAdLoaded() {
+                // Ad successfully loaded.
+            }
+            @Override
+            public void onRewardedAdFailedToLoad(LoadAdError adError) {
+                // Ad failed to load.
+            }
+        };
+        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
+    }
+
+    //Load rewarded ad
+    private void loadRewardedVideoAd() {
+        if (rewardedAd.isLoaded()) {
+            Activity activityContext = MainActivity.this;
+            RewardedAdCallback adCallback = new RewardedAdCallback() {
+                @Override
+                public void onRewardedAdOpened() {
+                    // Ad opened.
+                }
+
+                @Override
+                public void onRewardedAdClosed() {
+                    // Ad closed.
+                }
+
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem reward) {
+                    // User earned reward.
+                    browser.loadUrl("javascript:awebapp.rewarded()");
+                }
+
+                @Override
+                public void onRewardedAdFailedToShow(AdError adError) {
+                    // Ad failed to display.
+                }
+            };
+            rewardedAd.show(activityContext, adCallback);
+
+        } else {
+            //Log.d("TAG", "The rewarded ad wasn't loaded yet.");
+        }
+        initRewardedAd();
+    }
+
+    //Opening Google Play app and find this app on market to rate
+    public void launchMarket() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            startActivity(myAppLinkToMarket);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Share this app
+    public void launchSharer(String msg, String titlemsg) {
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, titlemsg);
+            String shareMessage = msg+"\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "Choose one"));
+        } catch(Exception e) {
+            //e.toString();
+        }
+    }
+
+    //Share a text
+    public void launchTextSharer(String msg, String titlemsg){
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, titlemsg);
+            String shareMessage = msg;
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "Choose one"));
+        } catch(Exception e) {
+            //e.toString();
+        }
     }
 
     public class WebAppInterface {
@@ -232,7 +378,17 @@ public class MainActivity extends AppCompatActivity {
 
         @JavascriptInterface
         public void removeAds() {
+            orderRemoveAds();
+        }
 
+        @JavascriptInterface
+        public void showRewardedAd(){
+            rewardedAdCanShow = true;
+        }
+
+        @JavascriptInterface
+        public void showAlert(String alerttext){
+            Toast.makeText(getApplicationContext(), alerttext, Toast.LENGTH_LONG).show();
         }
 
         @JavascriptInterface
@@ -246,6 +402,99 @@ public class MainActivity extends AppCompatActivity {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
 
+        @JavascriptInterface
+        public void rateThisApp(){
+            launchMarket();
+        }
+
+        @JavascriptInterface
+        public void shareText(String txt, String title){
+            launchTextSharer(txt, title);
+        }
+
+        @JavascriptInterface
+        public void shareThisApp(String txt, String title){
+            launchSharer(txt, title);
+        }
+
     }
+
+    //IBillingHandler implementation
+    @Override
+    public void onBillingInitialized() {
+        /*
+         * Called when BillingProcessor was initialized and it's ready to purchase
+         */
+    }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+        /*
+         * Called when requested PRODUCT ID was successfully purchased
+         */
+        if(productId.equals("noads")){
+            bp.consumePurchase("noads");
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean("ispro", true);
+            editor.apply();
+            ispro = true;
+            destroyBanner();
+            browser.loadUrl("javascript:awebapp.itspro(1)");
+        }
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+        /*
+         * Called when some error occurred. See Constants class for more details
+         *
+         * Note - this includes handling the case where the user canceled the buy dialog:
+         * errorCode = Constants.BILLING_RESPONSE_RESULT_USER_CANCELED
+         */
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+        /*
+         * Called when purchase history was restored and the list of all owned PRODUCT ID's
+         * was loaded from Google Play
+         */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    //End of IBillingHandler
+
+    //Remove Ads (purchase)
+    public void orderRemoveAds(){
+        boolean isAvailable = BillingProcessor.isIabServiceAvailable(MainActivity.this);
+        if(isAvailable) {
+            bp.purchase(MainActivity.this, "noads");
+        }
+    }
+
+    //OnDestroy etc...
+    @Override
+    protected void onDestroy() {
+        if (bp != null) {
+            bp.release();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
 
 }
